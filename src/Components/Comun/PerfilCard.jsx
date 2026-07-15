f186b2c6bceab239c3473f27702d82eb109e1c10
WebAntonioRodrigues <webantoniosilvards@gmail.com>
2026-07-15 19:41:36 +0100

    classe barra css

--- src/Components/Comun/PerfilCard.jsx
+++ src/Components/Comun/PerfilCard.jsx
@@ -1,4 +1,5 @@
 import '../../Styles/Comun/PerfilCard.css';
+import '../../Styles/Comun/barra.css';
 
 function PerfilCard({ foto, nome, cargo, dados }) {
 	return (
@@ -9,12 +10,13 @@ function PerfilCard({ foto, nome, cargo, dados }) {
 			<div className='perfil-info'>
 				<h1 className='perfil-nome'>{nome}</h1>
 				<p className='perfil-cargo'>{cargo}</p>
+				<div className='barra-dourada'></div>
 				<div className='perfil-dados'>
 					{dados.map(dado => (
 						<div key={dado.label} className='perfil-dado'>
 							<span className='perfil-dado-label'>{dado.label}</span>
-                            <span className='perfil-dado-valor'>{dado.valor}</span>
-                            <br />
+							<span className='perfil-dado-valor'>{dado.valor}</span>
+							<br />
 							<span className='perfil-dado-label'>{dado.eleicao}</span>
 							<span className='perfil-dado-valor'>{dado.eleicao1}</span>
 							<span className='perfil-dado-valor'>{dado.eleicao2}</span>
--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -2,6 +2,7 @@ import PerfilCard from '../../Components/Comun/PerfilCard';
 import logo from '../../assets/Logos/logo-banana.png';
 import '../../Styles/Home/Hero.css';
 
+
 function Hero() {
     return (
 			<section>
@@ -9,7 +10,7 @@ function Hero() {
 					<PerfilCard
 						foto={logo}
 						nome='RUI COSTA'
-						cargo='PRESIDENTE DO SL BENFICA'
+                    cargo='PRESIDENTE DO SL BENFICA'
 						dados={[{ label: 'Início de mandato', valor: '9 de outubro de 2021', eleicao: 'Eleições', eleicao1: '9 Outubro 2021 ( 84,48% )', eleicao2: '8 Novembro 2025 ( 65,89% )' }]}
 					/>
 				</div>
