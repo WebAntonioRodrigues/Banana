4071c004956a421f42c7e861e4e686d34fb301c2
WebAntonioRodrigues <webantoniosilvards@gmail.com>
2026-09-02 20:25:15 +0100

    footer updated

--- src/Components/Comun/Footer.jsx
+++ src/Components/Comun/Footer.jsx
@@ -1,14 +1,11 @@
-import "../../Styles/Comun/footer.css"
-
+import '../../Styles/Comun/footer.css';
 
 function Footer() {
-    
-    return (
-        <div>
-            <p className="footer-info">Dados atualizados até 13 de Julho de 2016</p>
-
-        </div>
-    )
+	return (
+		<div>
+			<p className='footer-info'>Dados atualizados até 13 de Julho de 2026</p>
+		</div>
+	);
 }
 
 export default Footer;
